8542b145c60affbda58a1a0426c63f8b9eff72e0
imAnonAmi <mcintireaw@gmail.com>
2020-05-14 13:12:50 -0700

    Made server file based on mini-project, but it won't work until I get db and models done.

---
+++ Develop/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+//Read up on morgan. Know it is required because of package.json, but not familiar with it.
+const logger = require("morgan");
+const mongoose = require("mongoose")
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+//ROUTE DEFINITIONS
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
+//LISTEN
+app.listen(PORT, () => {
+    console.log('Application is running on port ${PORT}.');
+});
