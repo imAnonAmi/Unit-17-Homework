e34e47764d3528f4159cc4553c8141f7482dd280
imAnonAmi <mcintireaw@gmail.com>
2020-05-16 14:19:40 -0700

    Modified server.js for heroku deploy.

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true}));
